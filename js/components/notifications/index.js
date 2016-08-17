31e5119301d763d6ef0a87124234b587feeb1858
Robert Shapiro <RobertShapiro@Roberts-MacBook-Pro.local>
2016-08-17 16:35:26 +0800

    Revert "fixed unread notifications"
    This reverts commit 3798248bb57238c6d4ef94e242de3cc54ba10e19.

--- js/components/notifications/index.js
+++ js/components/notifications/index.js
@@ -93,11 +93,6 @@ class Notifications extends Component {
         notify.note = checkInNote;
         notify.date = timestamp;
 
-        realm.write(() => {
-          notifications[i].b_isRead = true;
-        });
-
-
         notifications_list.push(notify);
       } // end of for loop
       this.setState({notification_list: notifications_list});
--- js/components/scanner/index.js
+++ js/components/scanner/index.js
@@ -148,8 +148,8 @@ class Scanner extends Component {
       if (users.length == 0)
         return;
 
-      var unread_notifications = realm.objects('NotificationModel').filtered('b_isRead == false').length;
-      this.setState({badgeCount:unread_notifications});
+      var notifications = realm.objects('NotificationModel').length;
+      this.setState({badgeCount:notifications});
 
       // get user access token
       var access_token = users[0].s_access_token;
